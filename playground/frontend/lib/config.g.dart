80ac7b7d1a4ba0aa71a99ba1c5a95d657657ebf9
rshamunov <ruslan.shamunov@akvelon.com>
2023-02-28 07:48:54 +0000

    DNS Changed

--- playground/frontend/lib/config.g.dart
+++ playground/frontend/lib/config.g.dart
@@ -1,39 +1,12 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
-// This file contains the backend URLs.
-//
-// On deployment it is re-generated with the actual URLs of a deployment
-// in /playground/terraform/build.gradle.kts in "prepareConfig" task.
-//
-// In the version-controlled copy, this file contains the production backend
-// URLs so developers can run the Playground locally without any configuration.
-// Changes to these version-controlled URLs do not affect deployment.
-//
-// Generated content below
 const String kAnalyticsUA = 'UA-73650088-2';
 const String kApiClientURL =
-      'https://router.stg3.pgtestsandbox.com';
+      'https://router.play-dev.beam.apache.org';
 const String kApiJavaClientURL =
-      'https://java.stg3.pgtestsandbox.com';
+      'https://java.play-dev.beam.apache.org';
 const String kApiGoClientURL =
-      'https://go.stg3.pgtestsandbox.com';
+      'https://go.play-dev.beam.apache.org';
 const String kApiPythonClientURL =
-      'https://python.stg3.pgtestsandbox.com';
+      'https://python.play-dev.beam.apache.org';
 const String kApiScioClientURL =
-      'https://scio.stg3.pgtestsandbox.com';
+      'https://scio.play-dev.beam.apache.org';
