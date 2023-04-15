3829c0efbd39611620defdf82a3501c3dc6c49ba
rshamunov <ruslan.shamunov@akvelon.com>
2023-04-15 16:47:02 +0400

    fixes

--- playground/terraform/infrastructure/cloudbuild-manual-setup/02.builders/secret.tf
+++ playground/terraform/infrastructure/cloudbuild-manual-setup/02.builders/secret.tf
@@ -25,3 +25,14 @@ resource "google_secret_manager_secret_version" "secret_gh_pat_cloudbuild_data" 
   secret = google_secret_manager_secret.secret_gh_pat_cloudbuild.id
   secret_data = var.data_for_github_pat_secret
 }
+
+resource "google_project_iam_binding" "secret_webhook_cloudbuild_trigger_cicd_access" {
+  project = var.project_id
+  secret_id = google_secret_manager_secret.secret_webhook_cloudbuild_trigger_cicd.secret_id
+  role    = "roles/secretmanager.secretAccessor"
+
+  members = [
+    "serviceAccount:${google_service_account.playground_ci_sa.email}",
+    "serviceAccount:${google_service_account.playground_cd_sa.email}"
+  ]
+}
